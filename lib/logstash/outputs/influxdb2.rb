6c77c66d70f272155c293e881589c2c93219ad2c
Issey Yamakoshi <r16turbo@gmail.com>
2023-10-28 23:59:44 +0900

    logstash-output-influxdb with 2.x support

--- lib/logstash/outputs/influxdb2.rb
+++ lib/logstash/outputs/influxdb2.rb
@@ -1,16 +1,86 @@
 # encoding: utf-8
 require "logstash/outputs/base"
+require "influxdb-client"
 
-# An influxdb2 output that does nothing.
 class LogStash::Outputs::InfluxDB2 < LogStash::Outputs::Base
   config_name "influxdb2"
 
+  # https://influxdata.github.io/influxdb-client-ruby/InfluxDB2/Client.html
+  config :url, :validate => :string, :required => true
+  config :token, :validate => :password, :required => true
+  config :options, :validate => :hash, :default => {}
+
+  # https://influxdata.github.io/influxdb-client-ruby/InfluxDB2/WriteOptions.html
+  config :write_options, :validate => :hash, :default => {}
+
+  config :measurement, :validate => :string, :required => true
+  config :tags, :validate => :string, :required => true
+  config :fields, :validate => :string, :required => true
+
+  config :include_tags, :validate => :string, :list => true, :default => []
+  config :exclude_tags, :validate => :string, :list => true, :default => []
+
+  config :escape_value, :validate => :boolean, :default => false
+
   public
   def register
+    @precision = @options.fetch("precision", InfluxDB2::DEFAULT_WRITE_PRECISION)
+    @client = InfluxDB2::Client.new(@url, @token.value, **_to_kwargs(@options))
+    write_options = InfluxDB2::WriteOptions.new(**_to_kwargs(@write_options))
+    @write_api = @client.create_write_api(write_options: write_options)
   end # def register
 
   public
   def receive(event)
-    return "Event received"
+    begin
+      tags = event.get(@tags)
+      unless @include_tags.empty?
+        tags = tags.select { |k,v| @include_tags.include?(k) }
+      end
+      unless @exclude_tags.empty?
+        tags = tags.select { |k,v| ! @exclude_tags.include?(k) }
+      end
+      fields = event.get(@fields)
+      unless @escape_value
+        fields = fields.transform_values { |v| ToStr.new(v) }
+      end
+      @write_api.write(data: InfluxDB2::Point.new(
+        name: event.sprintf(@measurement), tags: tags, fields: fields,
+        time: event.timestamp.time, precision: @precision))
+    rescue InfluxDB2::InfluxError => ie
+      @logger.warn("HTTP communication error while writing to InfluxDB", :exception => ie)
+    rescue Exception => e
+      @logger.warn("Non recoverable exception while writing to InfluxDB", :exception => e)
+    end
   end # def event
+
+  def close
+    @client.close!
+  end
+
+  def _to_kwargs(hash)
+    hash.map do |k,v|
+      case v
+      when "true"  then v = true
+      when "false" then v = false
+      end
+      [k.to_sym, v]
+    end.to_h
+  end
+
+  # prevents auto escape by client library.
+  class ToStr
+    def initialize(obj)
+      @obj = obj
+    end
+
+    def to_s
+      @obj.to_s
+    end
+
+    def to_str
+      @obj.to_str
+    end
+  end
+
 end # class LogStash::Outputs::InfluxDB2
--- logstash-output-influxdb2.gemspec
+++ logstash-output-influxdb2.gemspec
@@ -3,10 +3,10 @@ Gem::Specification.new do |s|
   s.version       = '0.1.0'
   s.licenses      = ['Apache-2.0']
   s.summary       = 'Logstash Output Plugin for InfluxDB2'
-  s.description   = 'TODO: Write a longer description or delete this line.'
-  s.homepage      = 'TODO: Put your plugin''s website or public repo URL here.'
-  s.authors       = ['your_username']
-  s.email         = 'your_username@example.com'
+  s.description   = 'This gem is a Logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/logstash-plugin install gemname. This gem is not a stand-alone program'
+  s.homepage      = 'https://github.com/r16turbo/logstash-output-influxdb2'
+  s.authors       = ['Issey Yamakoshi']
+  s.email         = 'r16turbo@gmail.com'
   s.require_paths = ['lib']
 
   # Files
@@ -18,7 +18,7 @@ Gem::Specification.new do |s|
   s.metadata = { "logstash_plugin" => "true", "logstash_group" => "output" }
 
   # Gem dependencies
-  s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.0"
-  s.add_runtime_dependency "logstash-codec-plain"
-  s.add_development_dependency "logstash-devutils"
+  s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.1"
+  s.add_runtime_dependency "influxdb-client", "~> 2.9"
+  s.add_development_dependency "logstash-devutils", "~> 2.5"
 end
